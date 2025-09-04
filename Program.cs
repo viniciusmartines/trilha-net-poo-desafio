9f5b1464f755241fee9556825012b12c804eaa16
Vinícius <martinesquaglio@gmail.com>
2025-09-04 16:08:29 -0300

    Adicionei atributos que faltavam na classe Smartphone seguindo o diagrama.png, implementei construtores e método abstrato InstalarAplicativos da classe Smartphone nas classes filhas. Realizei simples testes em Program.cs.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia n1 = new Nokia(numero: "1199845-0987", modelo: "Nokia1000", imei: "012345679111111", 32);
+n1.InstalarAplicativo("Whatsapp");
+n1.Ligar();
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone p1 = new Iphone("1193454-3453", "Iphone 15 Pro Max", "111111111111111", 256);
+p1.InstalarAplicativo("Facebook");
+p1.ReceberLigacao();
